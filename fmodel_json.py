04858bd8ce6ec9909f95e0c3e33d5fd11b92189e
mattwright324 <mattwright324@gmail.com>
2026-07-04 08:43:11 -0400

    latest py script

--- fmodel_json.py
+++ fmodel_json.py
@@ -4,6 +4,8 @@ import csv
 
 # --- CONFIG ---
 INPUT_FOLDER = r"C:\Users\mattwright324\AppData\Local\Temp\7zO01F3A62F\Output\Exports\ProjectRLH\Content\Levels\Map01\map01_p_WP\_Generated_"   # folder containing .json files
+#INPUT_FOLDER = r"C:\Users\mattwright324\AppData\Local\Temp\7zO01F3A62F\Output\Exports\ProjectRLH\Content\Levels\Map02\map02_p_WP\_Generated_"   # folder containing .json files
+#INPUT_FOLDER = r"C:\Users\mattwright324\AppData\Local\Temp\7zO01F3A62F\Output\Exports\ProjectRLH\Content\Levels\Map03\map03_p_WP\_Generated_"   # folder containing .json files
 OUTPUT_CSV   = r"C:\\Users\\mattwright324\\Desktop\\map01_components.csv"
 
 def parse_file(objects):
@@ -12,14 +14,30 @@ def parse_file(objects):
     Returns rows: (Type, SceneObjectName, StaticMeshName, X, Y, Z)
     """
 
+    ai_spawners = []
+    spawn_chances = []
+
     scene_components = []
     static_mesh_components = []
+    instanced_static_mesh_components = []
 
     for obj in objects:
+        obj_type = obj.get("Type")
+        props = obj.get("Properties", {})
+
+        if "AISpawner" in obj_type:
+            name = obj.get("Name")
+            spawner_name = props.get("SpawnChancesSet", {}).get("ObjectName")
+            ai_spawners.append([obj_type, name, spawner_name]) 
+
+        if "InitialSpawnChance" in props:
+            name = obj.get("Name")
+            spawn_chance = props.get("InitialSpawnChance", "")
+            spawn_chances.append([obj_type, name, spawn_chance])
+
 
         # Only process SceneComponent or StaticMeshComponent
-        obj_type = obj.get("Type")
-        if not (obj_type == "SceneComponent" or obj_type == "StaticMeshComponent"):
+        if not (obj_type == "SceneComponent" or obj_type == "StaticMeshComponent" or obj_type == "InstancedStaticMeshComponent"):
             continue
 
         # --- StaticMeshComponent ---
@@ -27,37 +45,93 @@ def parse_file(objects):
             outer = obj.get("Outer", {})
             sm_outer_name = outer.get("ObjectName", "")
 
-            props = obj.get("Properties", {})
             sm = props.get("StaticMesh", {})
             sm_name = sm.get("ObjectName", "")
 
             rel_loc = props.get("RelativeLocation", {})
+            if "X" not in rel_loc:
+                continue;
             x = rel_loc.get("X", "")
             y = rel_loc.get("Y", "")
             z = rel_loc.get("Z", "")
 
             static_mesh_components.append(
-                ("StaticMeshComponent", sm_outer_name, sm_name, x, y, z)
+                ["StaticMeshComponent", sm_outer_name, sm_name, x, y, z]
             )
+
+        # --- InstancedStaticMeshComponent ---
+        if obj_type == "InstancedStaticMeshComponent":
+            outer = obj.get("Outer", {})
+            sm_outer_name = outer.get("ObjectName", "")
+
+            sm = props.get("StaticMesh", {})
+            sm_name = sm.get("ObjectName", "")
+
+            # rel_loc = props.get("CachedBounds", {})
+            # value = rel_loc.get("Value", {})
+            # origin = value.get("Origin", {})
+            # if "X" not in origin:
+            #     continue;
+            # x = origin.get("X", "")
+            # y = origin.get("Y", "")
+            # z = origin.get("Z", "")
+
+            colls = []
+
+            body_instance = obj.get("BodyInstance", {})
+            coll_responses = body_instance.get("CollisionResponses", {})
+            res_arr = coll_responses.get("ResponseArray", [])
+            for coll in res_arr:
+                if not coll.get("Response") == "ECollisionResponse::ECR_Ignore":
+                    colls.append(coll.get("Channel", ""))
+
+            if not colls:
+                continue;
+
+            per_instance = obj.get("PerInstanceSMData", [])
+            for instance in per_instance:
+                transform = instance.get("TransformData", {})
+                translation = transform.get("Translation", {})
+                if "X" not in translation:
+                    continue;
+                x = translation.get("X", "")
+                y = translation.get("Y", "")
+                z = translation.get("Z", "")
+                instanced_static_mesh_components.append(
+                    ["InstancedStaticMeshComponent", sm_outer_name, sm_name, x, y, z]
+                )
 
         # --- SceneComponent ---
         elif obj_type == "SceneComponent":
             outer = obj.get("Outer", {})
             scene_name = outer.get("ObjectName", "UNKNOWN")
 
-            props = obj.get("Properties", {})
             rel_loc = props.get("RelativeLocation", {})
-
+            if "X" not in rel_loc:
+                continue;
             x = rel_loc.get("X", "")
             y = rel_loc.get("Y", "")
             z = rel_loc.get("Z", "")
 
             scene_components.append(
-                ("SceneComponent", scene_name, "", x, y, z)
+                ["SceneComponent", scene_name, "", x, y, z]
             )
+    
 
     # Combine both lists
-    return scene_components + static_mesh_components
+    combined_lists = scene_components + static_mesh_components + instanced_static_mesh_components
+
+    for arr in combined_lists:
+        result = ""
+        for spawner in ai_spawners:
+            if spawner[1] in (arr[1] or ""):
+                arr[2] = spawner[2]
+        for chance in spawn_chances:
+            if chance[1] in (arr[1] or "") or chance[1] in (arr[2] or ""):
+                result = chance[2]        
+        arr.append(result)
+
+    return combined_lists
 
 
 def main():
